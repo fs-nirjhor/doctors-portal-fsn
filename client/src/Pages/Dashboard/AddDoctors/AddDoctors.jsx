65c919c2316cafb7147dfb130827e93085ea33a0
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-08-25 20:01:17 +0600

    sent to server

--- client/src/Pages/Dashboard/AddDoctors/AddDoctors.jsx
+++ client/src/Pages/Dashboard/AddDoctors/AddDoctors.jsx
@@ -1,9 +1,68 @@
-
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import axios from 'axios';
 
 const AddDoctors = () => {
-  return (
-    <div>AddDoctors</div>
-  )
-}
+  const [info, setInfo] = useState({});
+  const [photo, setPhoto] = useState(null);
 
-export default AddDoctors
+  const handleInfoChange = (e) => {
+    const newInfo = { ...info };
+    newInfo[e.target.name] = e.target.value;
+    setInfo(newInfo);
+  };
+  const handlePhotoChange = (e) => {
+    const newPhoto = e.target.files[0];
+    setPhoto(newPhoto);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("photo", photo);
+    formData.append("name", info.name);
+    formData.append("email", info.email);
+
+      try {
+        const response = await axios.post("http://localhost:5000/add-doctors", formData);
+        console.log(formData)
+      } catch (error) {
+        console.log(error);
+      }
+  };
+
+  return (
+    <Container>
+      <h1 className="h1 my-5 text-center">Add a Doctor</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="doctor-name"
+          className="form-control mb-3"
+          placeholder="Doctor's Name"
+          onChange={handleInfoChange}
+        />
+        <input
+          type="email"
+          name="email"
+          className="form-control mb-3"
+          placeholder="Doctor's Email"
+          onChange={handleInfoChange}
+        />
+        <input
+          type="file"
+          name="photo"
+          className="form-control mb-3"
+          placeholder="Doctors Photo"
+          onChange={handlePhotoChange}
+        />
+        <button className="btn btn-primary ms-auto d-block" type="submit">
+          ADD
+        </button>
+      </form>
+    </Container>
+  );
+};
+
+export default AddDoctors;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
-require('dotenv').config()
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static("doctors"));
+app.use(fileUpload());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal-fsn.5lopyzi.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -18,36 +21,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const appointmentCollection= client.db("doctorsPortal").collection("appointment");
- 
- // post 
- app.post("/add-appointment", async (req, res) => {
+    const appointmentCollection = client
+      .db("doctorsPortal")
+      .collection("appointment");
+
+    // post
+    app.post("/add-appointment", async (req, res) => {
       const appointment = req.body;
       const result = await appointmentCollection.insertOne(appointment);
-      console.log(req.body.service + ' appointment successful!');
+      console.log(req.body.service + " appointment successful!");
       res.send(!!result.insertedId);
     });
-    
-    app.get("/appointment-by-date", async(req,res) => {
-    //  const date = req.body ;
-    const date = {date: req.query.date};
-      console.log('date', date);
-      const result = await  appointmentCollection.find(date).toArray();
-     console.log(result);
-     // res.send(result[0]);
+
+    app.get("/appointment-by-date", async (req, res) => {
+      //  const date = req.body ;
+      const date = { date: req.query.date };
+      console.log("date", date);
+      const result = await appointmentCollection.find(date).toArray();
+      console.log(result);
+      // res.send(result[0]);
       res.send(result);
-    })
- 
- // pinged to mongodb
+    });
+
+    app.post("/add-doctors", (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const photo = req.files.photo;
+      console.log(name, email, photo);
+    });
+
+    // pinged to mongodb
     await client.db("doctorsPortal").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,11 +69,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Backend Server of DOCTORS PORTAL FSN')
-})
+app.get("/", (req, res) => {
+  res.send("Backend Server of DOCTORS PORTAL FSN");
+});
 
 app.listen(port, () => {
-  console.log(`Doctors Portal Server is running on port ${port}`);
-})
+  console.log(`Doctors Portal Server is running on port http://localhost:5000`);
+});
